343fc61094822d9f4339e28c66c4f057ae355b2b
Saurabh Puri <saurabhpuri55@gmail.com>
2022-06-03 00:09:48 +0530

    Solve

---
+++ vanya_and_cubes.cpp
@@ -0,0 +1,27 @@
+
+/*
+Vanya got n cubes. He decided to build a pyramid from them. Vanya wants to build the pyramid as follows:
+ the top level of the pyramid must consist of 1 cube, the second level must consist of 1 + 2 = 3 cubes, 
+ the third level must have 1 + 2 + 3 = 6 cubes, and so on. 
+ Thus, the i-th level of the pyramid must have 1 + 2 + ... + (i - 1) + i cubes.
+Vanya wants to know what is the maximum height of the pyramid that he can make using the given cubes.
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+	int n;
+	cin >> n;
+	int count = 0;
+	int i = 0 ;
+	while (count <= n)
+	{
+		i++;
+		count += (i * (i + 1)) / 2;
+	}
+ 
+	cout << i - 1;
+}
