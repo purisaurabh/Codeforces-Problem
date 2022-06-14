80d5b839c33f3ee4ca46183d2226baf16dbd5902
Saurabh <saurabhpuri55@gmail.com>
2022-06-14 23:57:59 +0530

    Solve

--- petr_and_books.cpp
+++ petr_and_books.cpp
@@ -6,19 +6,19 @@ int main()
     int n;
     cin >> n;
     int arr[7];
-    for (int i = 0; i < 7; i++)
+    for(int i = 0 ; i < 7 ; i++)
     {
         cin >> arr[i];
     }
-
+    
     int i = 0;
-    while (n > 0)
+    while(n > 0)
     {
         n = n - arr[i];
-        if (n <= 0)
+        if(n <= 0)
             break;
         i++;
         i = i % 7;
     }
-    cout << i + 1 << endl;
+    cout << i + 1<< endl;
 }
