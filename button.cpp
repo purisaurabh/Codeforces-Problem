4349204eeafacde3f439cff80763c2025c1c05f7
Saurabh <saurabhpuri55@gmail.com>
2022-06-17 08:45:50 +0530

    Solve

---
+++ button.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int s = n;
+    for(int i = 1; i < n ; i++)
+        s += (n-i)*i;
+        
+    cout << s << endl;
+    return 0;
+}
