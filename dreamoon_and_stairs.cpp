339ca9bbd9e0f6bd3adfdf0a2d1008d66f66b25d
Saurabh <saurabhpuri55@gmail.com>
2022-06-18 09:15:22 +0530

    Solve

---
+++ dreamoon_and_stairs.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int a , b;
+        cin >> a >> b;
+        int count = 0;
+        
+        if( b > a)
+        {
+            count = -1;
+        }
+    
+        else
+        {
+            if(a%2)
+                count = a/2 + 1;
+                
+            else
+                count = a/2;
+                
+            while(count%b > 0)
+            {
+                count++;
+            }
+        }
+        
+        cout << count << endl;
+}
