780ef30f5238f25aaac99e4872aad0a6e51a8f38
Saurabh <saurabhpuri55@gmail.com>
2022-06-22 08:53:45 +0530

    Solve

---
+++ city_day.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,x,y;
+    cin >> n >> x >> y;
+    int arr[n];
+    
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    for(int i = 0 ; i < n ; i++)
+    {
+        bool flag = false;
+        
+        for(int j = i-1 ; j >= i-x && j >= 0 && !flag; j--)
+        {
+            if(arr[j] <= arr[i])
+            {
+                flag = true;
+            }
+        }
+        
+        if(flag)
+            continue;
+            
+        for(int j = i + 1; j < n && j <= i+y && !flag; j++)
+        {
+            if(arr[j] <= arr[i])
+                flag = true;
+        }
+        
+        if(!flag)
+        {
+            cout << i+1 << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
