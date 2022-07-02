d14f51b5b49d8f24bbd8519744fbf5650a057f58
Saurabh <saurabhpuri55@gmail.com>
2022-07-02 21:54:17 +0530

    Solve

--- Balanced_rating_changes.cpp
+++ Balanced_rating_changes.cpp
@@ -8,25 +8,25 @@ int main()
     cin >> n;
     vector <int> v(n);
     
-    int odd_number = 0;
+    int odd_num = 0;
     for(int i = 0 ; i < n ; i++)
     {
         cin >> v[i];
         if(v[i] % 2)
         {
-            odd_number++;
+            odd_num++;
         }
     }
     
-    odd_number = odd_number / 2;
+    odd_num = odd_num / 2;
     
     for(auto i : v)
     {
         if(i % 2)
         {
-            if(odd_number > 0) 
+            if(odd_num > 0) 
             {
-                odd_number--;
+                odd_num--;
                 i--;
             }
             
